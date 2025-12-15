10eadcb02aff6028de8d7dc5621bf664e3a147c0
PDY <pdy127001@gmail.com>
2025-12-15 15:29:06 +0900

    feat : add Racing base

---
+++ Source/team9_project/Private/MiniGame/Racing/T9_RacingGameMode.cpp
@@ -0,0 +1,16 @@
+#include "MiniGame/Racing/T9_RacingGameMode.h"
+
+void AT9_RacingGameMode::StartGame() 
+{
+
+}
+
+void AT9_RacingGameMode::EndGame()
+{
+
+}
+
+bool AT9_RacingGameMode::CheckGame()
+{
+	return false;
+}
