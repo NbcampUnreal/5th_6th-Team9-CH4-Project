840cebd7d5b54769d6d7113d2be422a3dcd48890
TheParkYB <park94128@naver.com>
2025-12-15 20:24:15 +0900

    [Add] 게임 모드 코드 파일

---
+++ Source/team9_project/GameMode/LobbyGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LobbyGameMode.generated.h"
+
+UCLASS()
+class TEAM9_PROJECT_API ALobbyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
