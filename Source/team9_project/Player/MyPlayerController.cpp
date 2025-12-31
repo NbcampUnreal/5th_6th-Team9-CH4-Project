82464f003d5d08d877583164cdf6a51a64b32f7f
05003 <sdj05003@gmail.com>
2025-12-31 16:12:43 +0900

    fix: inventory & HUD BP

--- Source/team9_project/Player/MyPlayerController.cpp
+++ Source/team9_project/Player/MyPlayerController.cpp
@@ -1,4 +1,4 @@
-#include "Player/MyPlayerController.h"
+﻿#include "Player/MyPlayerController.h"
 #include "Ui/Test/MyTestGameMode.h"
 #include "Ui/UIManagerSubsystem.h"
 #include "Ui/MinimapCameraActor.h"
@@ -75,7 +75,6 @@ void AMyPlayerController::BeginPlay()
     }
 
     // 4. 인벤토리 위젯 생성은 주석 처리 (GameHUDWidget 버튼으로 토글)
-    /*
     if (InventoryWidgetClass)
     {
         InventoryWidget = CreateWidget<UUserWidget>(this, InventoryWidgetClass);
@@ -84,7 +83,6 @@ void AMyPlayerController::BeginPlay()
             InventoryWidget->AddToViewport();  // 중복 생성 문제 해결됨
         }
     }
-    */
 }
 
 void AMyPlayerController::Server_RequestThrowDice_Implementation()
